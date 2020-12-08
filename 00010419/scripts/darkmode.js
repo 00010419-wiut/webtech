e7c32e0d73994cd00b63f81d611216c3a111ab27
00010419-wiut <72730808+00010419-wiut@users.noreply.github.com>
2020-12-08 19:22:25 +0500

    Add files via upload

---
+++ 00010419/scripts/darkmode.js
@@ -0,0 +1,8 @@
+
+
+/// https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp for dark mode solution 
+
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
